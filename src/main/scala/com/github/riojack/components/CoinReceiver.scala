f3da6f11af16d8b6247cee65e07db9f3869be331
AJ W <ayjay122@gmail.com>
2016-09-19 21:11:07 -0500

    Refactor: re-order logic for matching different coins.

--- src/main/scala/com/github/riojack/components/CoinReceiver.scala
+++ src/main/scala/com/github/riojack/components/CoinReceiver.scala
@@ -4,11 +4,11 @@ import com.github.riojack.domain.Coin
 
 case class CoinReceiver(pennies: Int = 0, nickels: Int = 0, dimes: Int = 0, quarters: Int = 0) {
   def putCoin(coin: Coin) = coin match {
-    case Coin(0, 0) => new CoinReceiver()
+    case Coin(5.0, 21.21) => new CoinReceiver(nickels = 1)
     case Coin(2.268, 17.91) => new CoinReceiver(dimes = 1)
     case Coin(5.670, 24.26) => new CoinReceiver(quarters = 1)
     case Coin(2.5, 19.05) => new CoinReceiver()
-    case _ => new CoinReceiver(nickels = 1)
+    case _ => new CoinReceiver()
   }
 }
 
