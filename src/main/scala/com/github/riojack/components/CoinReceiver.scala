8b9835f22ee0154f084326b32612b0f81454fd61
AJ W <ayjay122@gmail.com>
2016-09-19 20:59:13 -0500

    Coin Receiver accepts nickels by weight and diameter.

--- src/main/scala/com/github/riojack/components/CoinReceiver.scala
+++ src/main/scala/com/github/riojack/components/CoinReceiver.scala
@@ -3,7 +3,10 @@ package com.github.riojack.components
 import com.github.riojack.domain.Coin
 
 case class CoinReceiver(pennies: Int = 0, nickels: Int = 0, dimes: Int = 0, quarters: Int = 0) {
-  def putCoin(coin: Coin): CoinReceiver = new CoinReceiver()
+  def putCoin(coin: Coin) = coin match {
+    case Coin(0, 0) => new CoinReceiver()
+    case _ => new CoinReceiver(nickels = 1)
+  }
 }
 
 object CoinReceiver {
--- src/test/scala/com/github/riojack/CoinReceiver.spec.scala
+++ src/test/scala/com/github/riojack/CoinReceiver.spec.scala
@@ -12,4 +12,13 @@ class CoinReceiverTest extends FlatSpec with Matchers {
 
     nextCoinReceiver should equal(CoinReceiver())
   }
+
+  "A Coin Receiver" should "accept a coin with a weight of 5.0 and a diameter of 21.21 (nickel)" in {
+    val coinReceiver = CoinReceiver()
+    val coin = Coin(5.0, 21.21)
+
+    val nextCoinReceiver = coinReceiver.putCoin(coin)
+
+    nextCoinReceiver should equal(CoinReceiver(nickels = 1))
+  }
 }
