eef31f06882c5d3acb3bbf34eb4b61eda33da709
AJ W <ayjay122@gmail.com>
2016-09-19 21:20:17 -0500

    Refactorings...
     * Move and rename Coin Receiver test to proper package.
     * Add "aliases" for nickel, dime, quarter, and penny.
     * Use coin "aliases" in test.

--- src/test/scala/com/github/riojack/components/CoinReceiverTest.scala
+++ src/test/scala/com/github/riojack/components/CoinReceiverTest.scala
@@ -1,6 +1,6 @@
 package com.github.riojack.components
 
-import com.github.riojack.domain.Coin
+import com.github.riojack.domain._
 import org.scalatest._
 
 class CoinReceiverTest extends FlatSpec with Matchers {
@@ -15,7 +15,7 @@ class CoinReceiverTest extends FlatSpec with Matchers {
 
   "A Coin Receiver" should "accept a coin with a weight of 5.0 and a diameter of 21.21 (nickel)" in {
     val coinReceiver = CoinReceiver()
-    val coin = Coin(5.0, 21.21)
+    val coin = Nickel
 
     val nextCoinReceiver = coinReceiver.putCoin(coin)
 
@@ -24,7 +24,7 @@ class CoinReceiverTest extends FlatSpec with Matchers {
 
   "A Coin Receiver" should "accept a coin with a weight of 2.268 and a diameter of 17.91 (dime)" in {
     val coinReceiver = CoinReceiver()
-    val coin = Coin(2.268, 17.91)
+    val coin = Dime
 
     val nextCoinReceiver = coinReceiver.putCoin(coin)
 
@@ -33,7 +33,7 @@ class CoinReceiverTest extends FlatSpec with Matchers {
 
   "A Coin Receiver" should "accept a coin with a weight of 5.670 and a diameter of 24.26 (quarter)" in {
     val coinReceiver = CoinReceiver()
-    val coin = Coin(5.670, 24.26)
+    val coin = Quarter
 
     val nextCoinReceiver = coinReceiver.putCoin(coin)
 
@@ -42,7 +42,7 @@ class CoinReceiverTest extends FlatSpec with Matchers {
 
   "A Coin Receiver" should "reject a coin with a weight of 2.500 and a diameter of 19.05 (penny)" in {
     val coinReceiver = CoinReceiver()
-    val coin = Coin(2.5, 19.05)
+    val coin = Penny
 
     val nextCoinReceiver = coinReceiver.putCoin(coin)
 
