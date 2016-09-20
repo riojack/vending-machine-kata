2a475b0eb939c471003a8369d4b1ee90ca737b22
AJ W <ayjay122@gmail.com>
2016-09-19 21:23:15 -0500

    Refactor: use "it" for second and all subsequent Coin Receiver tests.

--- src/test/scala/com/github/riojack/components/CoinReceiverTest.scala
+++ src/test/scala/com/github/riojack/components/CoinReceiverTest.scala
@@ -13,7 +13,7 @@ class CoinReceiverTest extends FlatSpec with Matchers {
     nextCoinReceiver should equal(CoinReceiver())
   }
 
-  "A Coin Receiver" should "accept a coin with a weight of 5.0 and a diameter of 21.21 (nickel)" in {
+  it should "accept a coin with a weight of 5.0 and a diameter of 21.21 (nickel)" in {
     val coinReceiver = CoinReceiver()
     val coin = Nickel
 
@@ -22,7 +22,7 @@ class CoinReceiverTest extends FlatSpec with Matchers {
     nextCoinReceiver should equal(CoinReceiver(nickels = 1))
   }
 
-  "A Coin Receiver" should "accept a coin with a weight of 2.268 and a diameter of 17.91 (dime)" in {
+  it should "accept a coin with a weight of 2.268 and a diameter of 17.91 (dime)" in {
     val coinReceiver = CoinReceiver()
     val coin = Dime
 
@@ -31,7 +31,7 @@ class CoinReceiverTest extends FlatSpec with Matchers {
     nextCoinReceiver should equal(CoinReceiver(dimes = 1))
   }
 
-  "A Coin Receiver" should "accept a coin with a weight of 5.670 and a diameter of 24.26 (quarter)" in {
+  it should "accept a coin with a weight of 5.670 and a diameter of 24.26 (quarter)" in {
     val coinReceiver = CoinReceiver()
     val coin = Quarter
 
@@ -40,7 +40,7 @@ class CoinReceiverTest extends FlatSpec with Matchers {
     nextCoinReceiver should equal(CoinReceiver(quarters = 1))
   }
 
-  "A Coin Receiver" should "reject a coin with a weight of 2.500 and a diameter of 19.05 (penny)" in {
+  it should "reject a coin with a weight of 2.500 and a diameter of 19.05 (penny)" in {
     val coinReceiver = CoinReceiver()
     val coin = Penny
 
