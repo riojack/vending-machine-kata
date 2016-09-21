b3956e20eae75c161113cc137ac7ba3d7a1ac985
AJ W <ayjay122@gmail.com>
2016-09-20 22:57:51 -0500

    Refactor: pass starting inventory explicitly.

--- src/main/scala/com/github/riojack/components/ProductPicker.scala
+++ src/main/scala/com/github/riojack/components/ProductPicker.scala
@@ -2,7 +2,7 @@ package com.github.riojack.components
 
 import com.github.riojack.domain._
 
-class ProductPicker(implicit inventory: ProductInventory) {
+class ProductPicker(startingInventory: ProductInventory) {
   private lazy val dispenser = ProductDispenser
 
   def giveMe(productName: String) = dispenser.dispense(productName)
--- src/test/scala/com/github/riojack/components/ProductPickerTest.scala
+++ src/test/scala/com/github/riojack/components/ProductPickerTest.scala
@@ -6,10 +6,10 @@ import scala.language.postfixOps
 import scala.util.Random._
 import com.github.riojack.domain._
 
-class ProductPickerTest extends FlatSpec with Matchers with BeforeAndAfter {
+class ProductPickerTest extends FlatSpec with Matchers {
   "A Product Picker" should "return nothing if there is no inventory regardless of the product" in {
-    implicit val inventory = ProductInventory()
-    val picker = new ProductPicker()
+    val inventory = ProductInventory()
+    val picker = new ProductPicker(inventory)
 
     val productName = alphanumeric.take(10).mkString
     val product = picker.giveMe(productName)
@@ -18,8 +18,8 @@ class ProductPickerTest extends FlatSpec with Matchers with BeforeAndAfter {
   }
 
   it should "return nothing if there is inventory but the product is not recognized" in {
-    implicit val inventory = ProductInventory(1, 1, 1)
-    val picker = new ProductPicker()
+    val inventory = ProductInventory(1, 1, 1)
+    val picker = new ProductPicker(inventory)
 
     val productName = alphanumeric.take(10).mkString
     val product = picker.giveMe(productName)
@@ -28,11 +28,12 @@ class ProductPickerTest extends FlatSpec with Matchers with BeforeAndAfter {
   }
 
   it should "return cola if the inventory has one or more colas" in {
-    implicit val inventory = ProductInventory(cola = 1 + nextInt(10))
-    val picker = new ProductPicker()
+    val inventory = ProductInventory(cola = 1 + nextInt(10))
+    val picker = new ProductPicker(inventory)
 
     val product = picker.giveMe("cola")
 
     product should be(Some(Cola))
   }
+
 }
